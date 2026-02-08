9bf622e941c70898a0652b5bbab752a55cf03607
mohitsatyarthiii <mohitsatyarthi11@gmail.com>
2026-02-08 19:25:42 +0530

    d

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",   // 🔥 FINAL LIVE BACKEND
+  baseURL: "https://api.kanjiquejewels.com",   // 🔥 FINAL LIVE BACKEND
   withCredentials: true, // MUST for cookies
   timeout: 10000,
   headers: {
